78c57de4586055f06175461c52e0c2f7e8409f49
Christine Contreras <christineec23@yahoo.com>
2021-09-07 16:42:12 -0500

    working

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,12 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :actors do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
